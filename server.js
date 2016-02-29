222e6eb66e7e507d341df047c3067fa033df9e1b
Taylor Huston <taylor.a.huston@gmail.com>
2016-02-28 23:19:07 -0700

    Set up route to get all users

--- server.js
+++ server.js
@@ -73,7 +73,18 @@ apiRouter.route('/users')
       });
 
     });
+  })
+
+//Get all users
+.get(function (req, res) {
+  User.find(function (err, users) {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(users);
   });
+});
 
 //Register routes
 app.use('/api', apiRouter);
